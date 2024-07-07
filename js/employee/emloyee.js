5259f9f21046807528fea8d4e00c01bd368724df
sangdink <sangdinh0212@gmail.com>
2024-07-07 23:29:17 +0700

    update UI

---
+++ js/employee/emloyee.js
@@ -0,0 +1,58 @@
+window.onload = function(){
+    new EmployeePage();
+}
+
+
+class EmployeePage {
+    pageTitle = "Quản lý nhân viên";
+    constructor(){
+        
+        this.initEvents();
+    }
+    /**
+     * Khởi tạo các sự kiện trong page3
+     * Author: DTSANG (06/07/2024)
+     */
+    initEvents(){
+        try {
+           // Click button add hiển thị form thêm mới:
+            document.querySelector("#btnAdd").addEventListener('click', this.btnAddOnClick);
+            // Rfresh dữ liệu:
+            document.querySelector("#btnRefresh").addEventListener('click', this.btnRefreshOnClick);
+            // Xuất khẩu:
+
+            // Xóa nhiều bản ghi:
+
+            // Ẩn form chi tiết:
+            document.querySelector("#btnCloseDialog").addEventListener('click', ()=>{
+                document.querySelector("#myForm").style.visibility = "hidden";
+            })
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+        loadData(){
+            
+        }
+
+        /**
+         * Click button add
+         * Author: DTSANG (06/07/2024)
+         */
+        btnAddOnClick() {
+            try {
+                // Hiển thị form thêm mới
+                // 1. Lấy ra element của form thêm mới:
+                const dialog = document.getElementById("myForm");
+                // 2. Set hiển thị form:
+                dialog.style.visibility = "visible";
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        btnRefreshOnClick(){
+
+        }
+        //.....
+}
